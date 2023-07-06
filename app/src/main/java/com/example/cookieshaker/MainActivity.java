858bf81e359cfd0bf7c42602dc9a9d86c5a03652
Nukufel <vogeln@bzz.ch>
2023-07-06 09:43:30 +0200

    added images

--- app/src/main/java/com/example/cookieshaker/MainActivity.java
+++ app/src/main/java/com/example/cookieshaker/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.cookieshaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import javax.xml.transform.Result;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button shopB = findViewById(R.id.ShopButton);
+
+        shopB.setOnClickListener(e -> {
+            Intent intent = new Intent(MainActivity.this, ShopActivity.class);
+            startActivity(intent);
+        });
     }
 }
