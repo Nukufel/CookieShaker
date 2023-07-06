da9eb68d8117913b510af166003b84f3fa43bc34
Nukufel <vogeln@bzz.ch>
2023-07-06 15:30:12 +0200

    counter workes

--- app/src/main/java/com/example/cookieshaker/MainActivity.java
+++ app/src/main/java/com/example/cookieshaker/MainActivity.java
@@ -2,18 +2,79 @@ package com.example.cookieshaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import javax.xml.transform.Result;
+import java.util.List;
+import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private SensorManager sensorManager;
+    private float acceleration;
+    private float currentAcceleration;
+    private float lastAcceleration;
+
+    private TextView cookieCTRLabel;
+    private static long cookieCTR;
+
+    private TextView cpsLabel;
+    private static int cps;
+    private SensorEventListener sensorListener;
+
+    private SharedPreferences sharedPref;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+
+        sensorListener = new SensorEventListener() {
+            SharedPreferences.Editor editor = sharedPref.edit();
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                lastAcceleration = currentAcceleration;
+
+                currentAcceleration = (float) Math.sqrt(x * x + y * y + z * z);
+                float delta = currentAcceleration - lastAcceleration;
+                acceleration = acceleration * 0.9f + delta;
+
+                if (acceleration > 1) {
+                    cookieCTR++;
+                    cookieCTRLabel.setText(String.valueOf(cookieCTR));
+                    editor.putLong("cookieCTR", cookieCTR);
+                    editor.apply();
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+        };
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
+        acceleration = 0;
+        currentAcceleration = SensorManager.GRAVITY_EARTH;
+        lastAcceleration = SensorManager.GRAVITY_EARTH;
+
 
         Button shopB = findViewById(R.id.ShopButton);
 
@@ -21,5 +82,28 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, ShopActivity.class);
             startActivity(intent);
         });
+
+        cookieCTR = sharedPref.getLong("cookieCTR", 0);
+        cookieCTRLabel = findViewById(R.id.CookieCounterMain);
+        cookieCTRLabel.setText(String.valueOf(cookieCTR));
+
+        cps = sharedPref.getInt("cps", 0);
+        cpsLabel = findViewById(R.id.cpsMain);
+        cpsLabel.setText(String.valueOf(cps));
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorListener,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        sensorManager.unregisterListener(sensorListener);
+        super.onPause();
+    }
+
 }
--- app/src/main/java/com/example/cookieshaker/ShopActivity.java
+++ app/src/main/java/com/example/cookieshaker/ShopActivity.java
@@ -2,13 +2,44 @@ package com.example.cookieshaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class ShopActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        Button backB = findViewById(R.id.BackButton);
+
+        backB.setOnClickListener(e -> {
+            Intent intent = new Intent(ShopActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
     }
+
+
+
+
+
+
+
+
+
+
 }
